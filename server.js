f78eefc4b874fabf807a74661363b627cf7f8732
wiffin <wiffinian@gmail.com>
2022-02-05 12:35:11 -0500

    add heroku

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { notes } = require('./db/db.json')
+const fs = require('fs');
+const path = require('path');
+const { get } = require('http');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
